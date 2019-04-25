33e1216463dfcc5804e77fa9c089f9b2ffb18377
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-25 08:31:36 +0200

    js: add funtion to add, register, view offer

--- src/assets/js/main_offer.js
+++ src/assets/js/main_offer.js
@@ -1,49 +1,60 @@
-var URL = "http://" + window.location.host
-function load_my_offers() {
-  $.ajax({
-    url:URL +  "/me/offers",
-    type: "GET",
-    dataType: 'json',
-    contentType: 'application/json',
-    crossDomain: true,
-    success: function(data) {
-      if (data == null) {
-        return;
-      }
-      data.forEach(listMyOffer);
-    }
+/*jshint esversion: 8 */
+async function load_my_offers() {
+  my_offers = await get_my_offers();
+  my_offers.forEach(async function(offer_id) {
+    all_details = await get_offer_details(offer_id);
+    add_offer_to_list(offer_id, all_details[0], all_details[1], all_details[2]);
   });
 }
 
-function listMyOffer(offer) {
-  var offer_element = '<div id="'+ offer.id +'"class="my-offer"><div class="my-offer__content">'+
-  '<div class="my-offer__image" style="background-image: url(https://source.unsplash.com/700x700/?product);">'+
-  '</div><div class="my-offer__body"><h3 class="my-offer__title">'+offer.name+
-  '</h3><p>'+offer.details+'</p><a class="btn-remove-my-offer"><span></span></a></div></div></div>'
+function add_offer_to_list(id, name, image_url, details) {
+  var offer_element = '<div id="' + id + '"class="my-offer"><div class="my-offer__content">' +
+    '<div class="my-offer__image" style="background-image: url(' + image_url + ');">' +
+    '</div><div class="my-offer__body"><h3 class="my-offer__title">' + name +
+    '</h3><p>' + details + '</p><a class="btn-remove-my-offer"><span></span></a></div></div></div>';
   $("#my-offers").append(offer_element);
 }
 
+async function get_my_offers() {
+  id = await App.get_id();
+  my_offers = promisify(cb => App.contract.get_offers(id, App.options, cb));
+  return await my_offers;
+}
+
+async function get_offer_details(product_id) {
+  offer_details = await promisify(cb => App.contract.all_goods(product_id, App.options, cb));
+  return [offer_details[3], offer_details[4], offer_details[5]];
+}
 
 function add_offer() {
   console.log("offer added");
-  var offer_data = {
-    "name": $('#offer-title').val(),
-    "details": $('#offer-description').val(),
-    "image_url": "https://via.placeholder.com/150",
-  }
-  listMyOffer(offer_data);
-  console.log(offer_data);
-  $.ajax({
-    url:  URL +  "/offers",
-    type: "POST",
-    data: JSON.stringify(offer_data),
-    dataType: 'json',
-    contentType: 'application/json',
-    crossDomain: true,
-    success: function(data) {
-      console.log(data);
-    }
-  });
+  register_offer(
+    $('#offer-title').val(),
+    "https://source.unsplash.com/random",
+    $('#offer-description').val(),
+  );
+
+}
+
+function register_offer(name, image_url, details) {
+  App.contract.create_offer(
+    name,
+    image_url,
+    details,
+    App.options,
+    function(err, result) {
+      if (!err) {
+        console.log("Offer created!");
+        add_offer_to_list(null, name, image_url, details);
+      } else {
+        console.log(err);
+      }
+    });
+
+}
+
+async function on_contract_loaded() {
+  load_my_offers();
 }
 
 (function($) {
@@ -51,7 +62,7 @@ function add_offer() {
   console.log('url', URL);
   // Document Ready
   $(document).ready(function() {
-    load_my_offers();
+    App.init(on_contract_loaded);
   });
 
 })(jQuery);
